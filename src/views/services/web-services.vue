509b70990a4eca3d32c79e5cb9066128bf815feb
HiFiBerry <info@hifiberry.com>
2025-07-17 18:09:27 +0200

    Fix import path for LastFMIntegration component in web services view

--- src/views/services/web-services.vue
+++ src/views/services/web-services.vue
@@ -18,7 +18,7 @@
 
 <script setup lang="ts">
 import AppBackRouter from '@/components/app-back-router.vue'
-import LastFMIntegration from '@/components/lastfm.vue'
+import LastFMIntegration from '@/components/app-lastfm.vue'
 </script>
 
 <style scoped lang="scss">

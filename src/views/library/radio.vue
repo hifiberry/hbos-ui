d66adb7890ca02dfb5ea09bb9b1f0ff6f0dcde7f
7hr4x <ivanjoelpollak@proton.me>
2025-09-30 12:04:14 +0200

    refactor: rename `app-radio-edit-popup.vue` to `AppRadioEditPopup.vue`

--- src/views/library/radio.vue
+++ src/views/library/radio.vue
@@ -120,7 +120,7 @@ import AppIcon from '@/components/app-icon.vue'
 import AppBackRouter from '@/components/app-back-router.vue'
 import AppSearch from '@/components/AppSearch.vue'
 import AppMarquee from '@/components/app-marquee.vue'
-import AppRadioEditPopup from '@/components/app-radio-edit-popup.vue'
+import AppRadioEditPopup from '@/components/AppRadioEditPopup.vue'
 import { useRadioStore, type RadioStation, type RadioFavorite } from '@/stores/radio'
 
 const radioStore = useRadioStore()

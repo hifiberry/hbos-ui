a7d99de689aa236175824f18389751ccd414212c
7hr4x <ivanjoelpollak@proton.me>
2025-10-03 11:56:54 +0200

    feat: add ContentBox component and implement it in the libary index view

---
+++ src/components/ContentBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contentBox">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.contentBox {
+  background: var(--background-card);
+  border-radius: 8px;
+  padding: 24px;
+  color: var(--color-body);
+
+  &:not(:last-of-type) {
+  margin-bottom: 55px;
+    @include media-down(lg) {
+      margin-bottom: 24px;
+    }
+  }
+}
+</style>

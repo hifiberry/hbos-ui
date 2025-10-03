fe15d46781755ed21b78b2e628a703925883fbf7
7hr4x <ivanjoelpollak@proton.me>
2025-10-03 12:43:33 +0200

    refactor(CustomSearchField): make the search field use the ContentBox component

--- src/components/CustomSearchField.vue
+++ src/components/CustomSearchField.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="app-search">
-    <input :type="type" :value="modelValue" :placeholder="placeholder" @input="onInput" />
-    <button class="search-btn">
-      <Icon icon="magnifying-glass-light" />
-    </button>
-    <button v-if="modelValue.length" class="clear-btn" @click="onClear">
-      <Icon icon="clear" />
-    </button>
-  </div>
+  <ContentBox>
+    <div class="customSearchFieldContent">
+      <input :type="type" :value="modelValue" :placeholder="placeholder" @input="onInput" />
+      <button class="search-btn">
+        <Icon icon="magnifying-glass-light" />
+      </button>
+      <button v-if="modelValue.length" class="clear-btn" @click="onClear">
+        <Icon icon="clear" />
+      </button>
+    </div>
+  </ContentBox>
 </template>
 
 <script setup lang="ts">
+import ContentBox from "@/components/ContentBox.vue"
 import { useDebounceFn } from '@vueuse/core'
 
 import Icon from '@/components/Icon.vue'
@@ -58,78 +61,16 @@ const onClear = () => {
 </script>
 
 <style scoped lang="scss">
-.app-search {
-  position: relative;
-  &:focus-within {
-    input {
-      @include media-down(md) {
-        width: calc(100vw - 70px);
-        height: 32px;
-        padding: 0 40px 0 20px;
-        opacity: 1;
-        transform: translateX(0);
-      }
-    }
-  }
-  &:not(:focus-within) {
-    .clear-btn {
-      display: none;
-    }
-  }
-  input {
-    height: 32px;
-    border: none;
-    border-radius: 10px;
-    font-size: 16px;
-    font-weight: 500;
-    padding: 0 40px;
+.customSearchFieldContent{
+  padding: 5px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  input{
     width: 100%;
-    background-color: var(--search-background);
-    box-shadow: $box-shadow-main-content;
-    color: var(--search-color);
-    &::placeholder {
-      color: $search-color-placeholder;
-    }
-    @include media-down(md) {
-      height: 0;
-      opacity: 0;
-      overflow: hidden;
-      transition:
-        max-height 0.3s ease,
-        opacity 0.3s ease,
-        transform 0.3s ease;
-      transform: translateX(10px);
-      width: 0;
-      padding: 0;
-      position: absolute;
-      top: -5px;
-      right: 40px;
-    }
-  }
-  .search-btn,
-  .clear-btn {
-    width: 30px;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    padding: 0;
-    outline: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .search-btn {
-    left: 5px;
-    @include media-down(md) {
-      position: static;
-    }
-  }
-  .clear-btn {
-    right: 0;
-    @include media-down(md) {
-      right: 45px;
-      top: -2px;
-    }
+    color: var(--body-color);
   }
 }
 </style>

6d47ebc4f582999857d2db367032acdd2b45b9f4
7hr4x <ivanjoelpollak@proton.me>
2025-10-03 15:06:25 +0200

    feat: add ContentBoxLink

---
+++ src/components/ContentBoxLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link :to="to">
+    <ContentBox class="contentBoxLink">
+      <slot></slot>
+    </ContentBox>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import ContentBox from "@/components/ContentBox.vue"
+
+const { to } = defineProps<{ to: any }>()
+</script>
+
+<style scoped lang="scss">
+.contentBox {
+  padding: 20px;
+  transition: all 0.2s ease;
+  &:hover{
+    transform: translateY(-2px);
+  }
+}
+</style>

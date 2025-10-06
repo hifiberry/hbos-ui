44cb2f8052b6315d14035d9c726b87ed93af4b78
7hr4x <ivanjoelpollak@proton.me>
2025-10-06 09:23:45 +0200

    feat: implement backrouter option for PageContent

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>
+    <BackRouter v-if="backrouterLink" :to="backrouterLink" class="backrouter">
+      {{ title }}
+    </BackRouter>
+    <h1 v-else="backrouterLink">
       {{ title }}
     </h1>
     <div class="content">
@@ -10,13 +13,15 @@
 </template>
 
 <script setup lang="ts">
+import BackRouter from '@/components/BackRouter.vue'
   defineProps<{
     title: String,
+    backrouterLink: String,
   }>()
 </script>
 
 <style scoped lang="scss">
-h1{
+h1, .backrouter{
   padding-bottom: 25px;
 }
 </style>

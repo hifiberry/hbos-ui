ec19098e1a94661d4cc4c291697c2cc2405455a2
7hr4x <ivanjoelpollak@proton.me>
2025-10-06 09:07:17 +0200

    feat: add PageContent component
    This component should be used to display any content on a web page. It
    has a title prop that can be set.

---
+++ src/components/PageContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      {{ title }}
+    </h1>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    title: String,
+  }>()
+</script>
+
+<style scoped lang="scss">
+h1{
+  padding-bottom: 25px;
+}
+</style>

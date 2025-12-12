7ebb1c28896c386b01f1ac3a6db951c7efbfe0a0
arcathrax <ivanjoelpollak@proton.me>
2025-12-12 16:57:11 +0100

    feat(crossover-filter): have multiple channels
    channel a-d are now supported and it can be switched between them

--- src/views/sound/crossover-design.vue
+++ src/views/sound/crossover-design.vue
@@ -4,8 +4,11 @@
   :backrouterLink="{ name: 'sound' }">
     <div class="main-container">
       <ContentBox>
+        <h2>
+          {{ currentChannel }}
+        </h2>
         <FilterGraph
-          :filters="filters"
+          :filters="currentFilterArray"
           :activeFilterId="activeFilterId"
           :showBandwidthLines="true"
           :sampleRate="48000"
@@ -16,6 +19,18 @@
           @drag-start="onDragStart"
           @drag-end="onDragEnd"
         />
+        <button @click="currentChannel='Channel A'">
+          Channel A
+        </button>
+        <button @click="currentChannel='Channel B'">
+          Channel B
+        </button>
+        <button @click="currentChannel='Channel C'">
+          Channel C
+        </button>
+        <button @click="currentChannel='Channel D'">
+          Channel D
+        </button>
       </ContentBox>
       <ContentBox>
         <button @click="addItemToFilters">
@@ -28,7 +43,7 @@
 
 <script setup lang="ts">
 /* IMPORTS */
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { type Filter } from '@/utils/filtercalc';
 import PageContent from '@/components/PageContent.vue'
 import ContentBox from '@/components/ContentBox.vue'
@@ -37,19 +52,50 @@ import FilterGraph from '@/components/FilterGraph.vue'
 
 /* GLOBAL DEFINITIONS */
 const activeFilterId = ref<number | null>(1)
-const filters = ref<Filter[]>([]);
+const channelAFilters = ref<Filter[]>([]);
+const channelBFilters = ref<Filter[]>([]);
+const channelCFilters = ref<Filter[]>([]);
+const channelDFilters = ref<Filter[]>([]);
+const currentChannel = ref<string>("Channel A");
+const currentFilterArray = computed(() => {
+  return getCurrentFilterArray();
+});
 
 
 /* FUNCTIONS */
+/**
+  * Returns the current filter array based on the `currentChannel` string.
+  * @returns {Filter[]} the `channelXFilters.value` where `X` is the current channels letter.
+  */
+function getCurrentFilterArray(): Filter[] {
+  switch (currentChannel.value) {
+    case "Channel A":
+      return channelAFilters.value;
+      break;
+    case "Channel B":
+      return channelBFilters.value;
+      break;
+    case "Channel C":
+      return channelCFilters.value;
+      break;
+    case "Channel D":
+      return channelDFilters.value;
+      break;
+
+    default:
+      return channelAFilters.value;
+      break;
+  }
+}
 
 /**
   * Adds a filter to the global `filters` array.
   */
 function addItemToFilters() {
   // Get current filter length so the id can be calculated later on
-  const filterLength = filters.value.length;
+  const filterLength = currentFilterArray.value.length;
 
-  filters.value.push({
+  currentFilterArray.value.push({
     id: filterLength+1,
     icon: 'peaking',
     text: 'Band ' + filterLength+1,
@@ -71,7 +117,7 @@ function addItemToFilters() {
   */
 const onUpdateFreqGain = ({ id, frequency, gain }) => {
   // Find the filter object that matches the supplied id.
-  const target = filters.value.find(f => f.id === id);
+  const target = currentFilterArray.value.find(f => f.id === id);
 
   // Return if no matching filter was found.
   if (!target) {
@@ -91,7 +137,7 @@ const onUpdateFreqGain = ({ id, frequency, gain }) => {
   */
 const onUpdateQ = ({ id, Q }) => {
   // Find the filter that matches the supplied id.
-  const target = filters.value.find(f => f.id === id);
+  const target = currentFilterArray.value.find(f => f.id === id);
 
   // Return if no matching filter was found
   if (!target) {

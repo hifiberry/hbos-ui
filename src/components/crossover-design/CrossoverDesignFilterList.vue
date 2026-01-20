8a2b5b1caa78011e8e7e339afbfb5b85689ccbbe
dystiv <ivanjoelpollak@proton.me>
2026-01-20 14:31:29 +0100

    feat: update styling and add message that no filter is in the current channel

--- src/components/crossover-design/CrossoverDesignFilterList.vue
+++ src/components/crossover-design/CrossoverDesignFilterList.vue
@@ -1,6 +1,9 @@
 <template>
   <ContentBox>
     <div class="filter-list-container">
+      <template v-if="props.filterList.length === 0">
+        <p>no filters in the current channel</p>
+      </template>
       <div v-for="filter in props.filterList" class="filter-list-entry">
         <Icon :icon="getFilterIconName(filter.icon)" />
         <p>
@@ -27,9 +30,21 @@ const props = defineProps<{
 }
 
 .filter-list-entry {
+  transition: all 0.25s;
+
+  border: 2px solid var(--color-body);
+  border-radius: 8px;
+  padding: 10px;
+  margin: 10px;
+
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  &:hover {
+    border: 2px solid var(--primary);
+    background: rgba(225, 30, 74, 0.1);
+  }
 }
 </style>

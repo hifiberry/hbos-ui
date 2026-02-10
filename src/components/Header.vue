6bf40f45e5ab11fb853ac360e52d2b1a62422dc9
dystiv <ivanpollak@ikmail.com>
2026-02-10 15:37:58 +0100

    fix(header): remove darkmode toggle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,36 +5,16 @@
       <SongControlInfo isOnHeader />
     </div>
 
-    <!-- Spacer -->
-    <div class="header-spacer"></div>
-
     <!-- Volume -->
     <div class="header-column header-column--volume">
       <VolumeControl size="compact" />
-    </div>
-
-    <!-- Spacer -->
-    <div class="header-spacer"></div>
-
-    <!-- Dark Mode Toggle -->
-    <div class="header-column header-column--actions">
-      <button @click="toggleDark()">
-        <svg class="dark-mode-icon" width="27" height="27" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
-        </svg>
-      </button>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core'
 import SongControlInfo from '@/components/SongControlInfo.vue'
 import VolumeControl from '@/components/VolumeControl.vue'
-
-// Dark mode
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
 </script>
 
 <style scoped lang="scss">
@@ -42,8 +22,8 @@ const toggleDark = useToggle(isDark)
   background-color: var(--background-header);
   padding: 20px 15px;
   border-bottom: 1px solid var(--color-header-border);
-  display: grid;
-  grid-template-columns: auto 1fr auto 1fr 80px; /* player | spacer | volume | spacer | actions */
+  display: flex;
+  justify-content: space-between;
   align-items: center;
   height: 80px;
   position: fixed;
@@ -72,9 +52,5 @@ const toggleDark = useToggle(isDark)
   &--actions {
     justify-content: flex-end;
   }
-}
-
-.header-spacer {
-  /* hidden on small screens via parent */
 }
 </style>
